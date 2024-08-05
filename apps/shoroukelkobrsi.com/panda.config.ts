099a57825415b366c7b6dde062e35389fd1fa1e4
Alice Alexandra Moore <86723305+3mdistal@users.noreply.github.com>
2024-08-05 08:50:21 -0400

    Panda up and running.

---
+++ apps/shoroukelkobrsi.com/panda.config.ts
@@ -0,0 +1,30 @@
+import { defineConfig, defineKeyframes } from "@pandacss/dev";
+
+const keyframes = defineKeyframes({
+  gradientAnimation: {
+    "0%": { backgroundPosition: "0% 0%" },
+    "50%": { backgroundPosition: "0% 100%" },
+    "100%": { backgroundPosition: "0% 0%" },
+  },
+});
+
+export default defineConfig({
+  // Whether to use css reset
+  preflight: true,
+
+  // Where to look for your css declarations
+  include: ["./src/**/*.{js,jsx,ts,tsx}"],
+
+  // Files to exclude
+  exclude: [],
+
+  // Useful for theme customization
+  theme: {
+    extend: {
+      keyframes,
+    },
+  },
+
+  // The output directory for your css system
+  outdir: "styled-system",
+});
--- apps/shoroukelkobrsi.com/src/components/reel.tsx
+++ apps/shoroukelkobrsi.com/src/components/reel.tsx
@@ -1,9 +1,29 @@
-import styles from "./reel.module.css";
+import { css } from "../../styled-system/css";
 
 export default function Reel(): React.ReactElement {
   return (
-    <div className={styles.reelContainer}>
-      <div className={styles.reel} />
+    <div
+      className={css({
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        marginTop: "calc(var(--menu-height) * -1)",
+        background: "var(--gray-12)",
+        width: "100%",
+        height: "100dvh",
+      })}
+    >
+      <div
+        className={css({
+          animation: "gradientAnimation 10s ease infinite",
+          background:
+            "linear-gradient(to bottom, var(--gray-10), var(--gray-2))",
+          backgroundSize: "100% 200%",
+          aspectRatio: "var(--ratio-widescreen)",
+          maxWidth: "100%",
+          height: "100%",
+        })}
+      />
     </div>
   );
 }
